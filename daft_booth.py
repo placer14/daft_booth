11ce9d261bf18f14e31bba742b92140ff9ad67bf
Mike Greenberg <mg@nobulb.com>
2013-11-15 00:28:07 -0500

    Reorganize sound files; minor code edit

--- daft_booth.py
+++ daft_booth.py
@@ -12,91 +12,51 @@ def main():
     print('Running...')
     pygame.mixer.init()
     pygame.init()
-    
+
     pygame.mixer.music.load(FULL_PATH + 'sounds/beat.mp3')
-    button_sound = []
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/CallIt.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/After1.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/Better1.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/BurnIt.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/ChangeIt.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/CodeIt.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/CrackIt.wav'))
-    button_sound.append(pygame.mixer.Sound(FULL_PATH + 'sounds/CutIt.wav'))
-    
+    work_it_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/101_work_it.wav')
+    harder_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/102_harder.wav')
+    make_it_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/103_make_it.wav')
+    better_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/104_better.wav')
+    do_it_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/105_do_it.wav')
+    faster_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/106_faster.wav')
+    makes_us_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/107_makes_us.wav')
+    stronger_A = pygame.mixer.Sound(FULL_PATH + 'sounds/hbfs/108_stronger.wav')
+
     pygame.mixer.music.play(-1)
-    
+
     # Just need screen for development, in order to easily get keyboard events.
     screen = pygame.display.set_mode([300, 100])
     screen.fill([255, 255, 255])
 
     while True:
-
-        # See commented out code below to get events from inputs.        
+        # See commented out code below to get events from inputs.
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 print('Quiting')
                 pygame.quit()
                 sys.exit()
-                
+
                 break
             elif event.type == pygame.KEYDOWN:
                 print('KeyPressed')
                 if (event.key == pygame.K_1):
-                    button_sound[0].play()
+                    work_it_A.play()
                 elif (event.key == pygame.K_2):
-                    button_sound[1].play()
+                    harder_A.play()
                 elif (event.key == pygame.K_3):
-                    button_sound[2].play()
+                    make_it_A.play()
                 elif (event.key == pygame.K_4):
-                    button_sound[3].play()
+                    better_A.play()
                 elif (event.key == pygame.K_5):
-                    button_sound[4].play()
+                    do_it_a.play()
                 elif (event.key == pygame.K_6):
-                    button_sound[5].play()
+                    faster_A.play()
                 elif (event.key == pygame.K_7):
-                    button_sound[6].play()
+                    makes_us_A.play()
                 elif (event.key == pygame.K_8):
-                    button_sound[7].play()
-                    
-        
+                    stronger_A.play()
+
+
 if __name__ == '__main__':
     main()
-    
-    
-    
-#mport pygame.mixer
-#from time import sleep
-#import RPi.GPIO as GPIO
-#from sys import exit
-#
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(23, GPIO.IN)
-#GPIO.setup(24, GPIO.IN)
-#GPIO.setup(25, GPIO.IN)
-#
-#pygame.mixer.init(48000, -16, 1, 1024)
-#
-#sndA = pygame.mixer.Sound("buzzer.wav")
-#sndB = pygame.mixer.Sound("clap.wav")
-#sndC = pygame.mixer.Sound("laugh.wav")
-#
-#soundChannelA = pygame.mixer.Channel(1)
-#soundChannelB = pygame.mixer.Channel(2)
-#soundChannelC = pygame.mixer.Channel(3)
-#
-#print "Sampler Ready."
-#
-#while True:
-#   try:
-#      if (GPIO.input(23) == True):
-#         soundChannelA.play(sndA)
-#      if (GPIO.input(24) == True):
-#         soundChannelB.play(sndB)
-#      if (GPIO.input(25) == True):
-#         soundChannelC.play(sndC)
-#      sleep(.01)
-#   except KeyboardInterrupt:
-#     exit()
- 
